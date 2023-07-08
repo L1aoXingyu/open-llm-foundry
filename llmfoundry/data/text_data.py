1e457de2b477aa76a84eada7cfeeab135c13d303
xingyu.liao <sherlockliao01@gmail.com>
2023-07-08 16:33:00 +0800

    fix resume training bug for dataloader shared memory

--- llmfoundry/data/text_data.py
+++ llmfoundry/data/text_data.py
@@ -320,8 +320,12 @@ if __name__ == '__main__':
     cfg = om.create(cfg)
     device_batch_size = 2
 
-    tokenizer_cfg = {'name': args.tokenizer, 'kwargs': {}}
-    tokenizer_cfg['kwargs'] = {'model_max_length': args.max_seq_len}
+    tokenizer_cfg = {
+        'name': args.tokenizer,
+        'kwargs': {
+            'model_max_length': args.max_seq_len
+        },
+    }
     tokenizer_cfg = om.create(tokenizer_cfg)
     tokenizer = build_tokenizer(tokenizer_cfg)
 
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 
 # Train an MPT-125m model for 10 batches
-composer train/train.py \
+python train/train.py \
   train/yamls/pretrain/santacoder.yaml
